d7d5a9560283b432cca57ea7c034ca2423fcfdc4
Molitor István <istvanmolitor@gmail.com>
2026-06-01 16:28:52 +0200

    scraper

---
+++ src/database/seeders/ArticleScraperSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace IstvanMolitor\ArticleScraper\database\seeders;
+
+use Illuminate\Database\Seeder;
+use Molitor\User\Exceptions\PermissionException;
+use Molitor\User\Services\AclManagementService;
+
+class ArticleScraperSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            /** @var AclManagementService $aclService */
+            $aclService = app(AclManagementService::class);
+            $aclService->createPermission('article-scraper', 'Cikk scraper kezelése', 'admin');
+        } catch (PermissionException $e) {
+            $this->command->error($e->getMessage());
+        }
+    }
+}
